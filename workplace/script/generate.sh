1b58725c906df5ec28522b317d2ebd6af2aeb97f
teanakamura <wayward.at23@gmail.com>
2019-12-03 12:00:05 +0900

    modify gitignore

--- workplace/script/generate.sh
+++ workplace/script/generate.sh
@@ -2,8 +2,8 @@ CURRENT_DIR=`pwd`
 SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR
 EXEC_FILE_PATH='../../fairseq/fairseq_cli/'
-DATA_DIR='../data-bin/cnndm_small/'
-SAVE_DIR='../checkpoints/cnndm_small/insertion_transformer/checkpoint_last.pt'
+DATA_DIR='../data-bin/cnndm/'
+SAVE_DIR='../checkpoints/cnndm/insertion_transformer/checkpoint_best.pt'
 
 CUDA_VISIBLE_DEVICES=2,3 \
    python ${EXEC_FILE_PATH}generate.py ${DATA_DIR} \
