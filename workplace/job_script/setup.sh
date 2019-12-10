dffb8461106fecdb3533f9692b6e7e12b542d25b
acb11655gd <acb11655gd@es4.abci.local>
2019-12-10 11:49:34 +0900

    modify job script

--- workplace/job_script/setup.sh
+++ workplace/job_script/setup.sh
@@ -5,26 +5,20 @@ cd $SCRIPT_DIR
 echo $SCRIPT_DIR
 pwd
 
-#$ -l rt_G.small=1
+#$ -l rt_F=1
 #$ -l h_rt=72:00:00
 #$ -j y
 #$ -cwd
 #$ -o std/out
 #$ -e std/err
 
-pwd 
-
 ## Initialize module
 source /etc/profile.d/modules.sh
 
 module load python/3.6/3.6.5
-#module load cuda/9.1/9.1.85.3
-module load cuda/10.1/10.1.243
-#module load cudnn/7.1/7.1.3
-module load cudnn/7.6/7.6.4
-#module load openmpi/2.1.6
-#module load nccl/2.1/2.1.15-1
+module load cuda/9.1/9.1.85.3
+module load cudnn/7.1/7.1.3
 
 source ~/venv/pytorch/bin/activate
 
-source ~/fairseq/workplace/script/train.sh
+source ~/fairseq/fairseq/setup.py build_ext --inplace
