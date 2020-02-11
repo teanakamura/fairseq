7214ebe5d8c809864c9a99231051dbb3235c560e
teanakamura <wayward.at23@gmail.com>
2020-02-11 19:12:43 +0900

    modify tf_train.sh

--- workplace/script/tf_train.sh
+++ workplace/script/tf_train.sh
@@ -11,9 +11,24 @@ do
   ls "$CONF_DIR" | grep -E ".+\.conf" --colour=never
   read "CONF_FILE?Input config file: "
 done
+echo "CONFIG FILE: $CONF_FILE"
 
 declare -A CONF # bash>=4.2
-echo "CONFIG FILE: $CONF_FILE"
+CONF[model]=transformer
+CONF[criterion]=cross_entropy
+CONF[max_source]=512
+CONF[max_target]=512
+CONF[lr]=0.0005
+CONF[min_lr]=1e-09
+CONF[warmup_updates]=10000
+CONF[warmup_init_lr]=1e-07
+CONF[dropout]=0.3
+CONF[weight_decay]=0.01
+CONF[max_tokens]=2048
+CONF[max_sentences]=16
+CONF[update_freq]=4
+CONF[max_update]=300000
+CONF[keep_last_epochs]=3
 
 while read line
 do
@@ -39,34 +54,34 @@ echo $SAVE_DIR
    python ${EXEC_FILE_PATH}train.py ${DATA_DIR} \
    --save-dir ${SAVE_DIR} \
    --arch transformer_cov \
-      --max-source-positions 512 \
-      --max-target-positions 512 \
+      --max-source-positions ${CONF[max_source]} \
+      --max-target-positions ${CONF[max_target]} \
    --task translation \
      --truncate-source \
    --ddp-backend=no_c10d \
    --criterion ${CONF[criterion]} \
    --optimizer adam \
       --adam-betas '(0.9,0.98)' \
-   --lr 0.0005 \
+   --lr ${CONF[lr]} \
    --lr-scheduler inverse_sqrt \
-   --min-lr '1e-09' \
-   --warmup-updates 10000 \
-   --warmup-init-lr '1e-07' \
-   --dropout 0.3 \
-   --weight-decay 0.01 \
+   --min-lr ${CONF[min_lr]} \
+   --warmup-updates ${CONF[warmup_updates]} \
+   --warmup-init-lr ${CONF[warmup_init_lr]} \
+   --dropout ${CONF[dropout]} \
+   --weight-decay ${CONF[weight_decay]} \
    --decoder-learned-pos \
    --encoder-learned-pos \
    --log-format 'simple' \
    --log-interval 1000 \
    --fixed-validation-seed 7 \
-   --max-tokens 2048 \
-   --max-sentences 16 \
-   --update-freq 4 \
+   --max-tokens ${CONF[max_tokens]} \
+   --max-sentences ${CONF[max_sentences]} \
+   --update-freq ${CONF[update_freq]} \
    --save-interval-updates 20000 \
-   --max-update 300000 \
+   --max-update ${CONF[max_update]} \
    --skip-invalid-size-inputs-valid-test \
    --user-dir ${USER_DIR} \
-   --keep-last-epochs 3 \
+   --keep-last-epochs ${CONF[keep_last_epochs]}\
    --truncate-source \
    --fp16 \
    --tensorboard-logdir ${TENSORBOARD_DIR} \
