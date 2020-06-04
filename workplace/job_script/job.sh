d56099c8eeee1276fef49d45940bd8c878d83010
teanakamura <wayward.at23@gmail.com>
2020-06-04 16:32:15 +0900

    change file structure in job_script directory

--- workplace/job_script/job.sh
+++ workplace/job_script/job.sh
@@ -15,16 +15,6 @@ pwd
 pwd 
 
 ## Initialize module
-source /etc/profile.d/modules.sh
-
-module load python/3.6/3.6.5
-#module load cuda/9.1/9.1.85.3
-module load cuda/10.1/10.1.243
-#module load cudnn/7.1/7.1.3
-module load cudnn/7.6/7.6.4
-#module load openmpi/2.1.6
-#module load nccl/2.1/2.1.15-1
-
-source ~/venv/pytorch/bin/activate
+source ~/fairseq/workplace/job_script/module_load.sh
 
 zsh ~/fairseq/workplace/script/$1 $2
---
+++ workplace/job_script/qrsh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+qrsh -l rt_F=1 -l h_rt=2:00:00
