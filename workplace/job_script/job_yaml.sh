22c3d0022c8cad80b607ce29a8a26ef1142ce163
teanakamura <wayward.at23@gmail.com>
2020-08-02 12:56:37 +0900

    submit fairseq trian task with yaml config file

---
+++ workplace/job_script/job_yaml.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+cd $SCRIPT_DIR
+pwd
+
+#$ -l rt_F=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$ -cwd
+#$ -o std/std0/out
+##$ -e std/std0/err
+
+## load jobrc
+source ~/fairseq/workplace/job_script/jobrc
+echo $PATH
+
+## Initialize module
+source ~/fairseq/workplace/job_script/module_load.sh
+
+zsh ~/fairseq/workplace/script/$1 $2 $3
