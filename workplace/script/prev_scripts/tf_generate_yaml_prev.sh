b538a5c57919bcdaa4d1e4827d7bc6c957dc55c1
teanakamura <wayward.at23@gmail.com>
2020-12-10 18:56:54 +0900

    update scripts

--- workplace/script/prev_scripts/tf_generate_yaml_prev.sh
+++ workplace/script/prev_scripts/tf_generate_yaml_prev.sh
@@ -1,51 +1,36 @@
+#!/bin/zsh
+
+update_conf () {
+  parse_yaml $1 |
+  while read line
+  do
+    if echo $line | grep -F = &>/dev/null
+    then
+      varname=$(echo "$line" | cut -d '=' -f 1)
+      CONF[$varname]=$(echo "$line" | cut -d '=' -f 2- | sed -e 's/^"//' -e 's/"$//' )
+    fi
+  done
+}
+
+
 echo ${JOB_ID}
 
-FAIRSEQ_ROOT=~/fairseq/
-CONF_DIR=${FAIRSEQ_ROOT}/workplace/script/configs
-CONF_FILE=$1
 
-while [ ! -f "$CONF_DIR/$CONF_FILE" ]
-do
-  ls "$CONF_DIR" | grep -E ".+\.conf" --colour=never
-  read "CONF_FILE?Input config file: "
-done
-echo "CONFIG FILE: $CONF_FILE"
+CONF_DIR=${FAIRSEQ_ROOT}/workplace/script/yaml_configs
+DATA=$1
+CONF_FILE=$2
+
 
 declare -A CONF # bash>=4.2
-CONF[model]=transformer
-CONF[arch]=transformer
-CONF[task]=translation
-CONF[criterion]=cross_entropy
-CONF[max_source]=512
-CONF[max_target]=512
-CONF[lr]=0.0005
-CONF[min_lr]=1e-09
-CONF[warmup_updates]=10000
-CONF[warmup_init_lr]=1e-07
-CONF[dropout]=0.3
-CONF[weight_decay]=0.01
-CONF[max_tokens]=2048
-CONF[max_sentences]=16
-CONF[update_freq]=4
-CONF[max_update]=300000
-CONF[keep_last_epochs]=3
-CONF[fp16]=true
-CONF[cpu]=false
-CONF[reset_optimizer]=false
-CONF[gen_subset]=test
-CONF[beam]=5
+update_conf $CONF_DIR/default.yml
+update_conf $CONF_DIR/$DATA/default.yml
+update_conf $CONF_DIR/$DATA/$CONF_FILE
+for key in ${(k)CONF[@]}; do  # zsh
+  echo "$key: ${CONF[$key]}"
+done
 
-while read line
-do
-  if echo $line | grep -F = &>/dev/null
-  then
-    varname=$(echo "$line" | cut -d '=' -f 1)
-    CONF[$varname]=$(echo "$line" | cut -d '=' -f 2-)
-  fi
-done < $CONF_DIR/$CONF_FILE
-CONF_FILE=
 
-ENV_FILE=${FAIRSEQ_ROOT}/workplace/script/env
+ENV_FILE=${FAIRSEQ_ROOT}/workplace/script/env_yaml
 source ${ENV_FILE}
 if [[ ${CONF[data]: -7} == subword ]]; then
   SYSTEM=system_output-subword.txt
@@ -63,17 +48,19 @@ OPTIONAL_ARGS=(
    --gen-subset ${CONF[gen_subset]}
    --path ${SAVE_FILE}
    --beam ${CONF[beam]} 
-   --task ${CONF[task]}
+   --task ${CONF[model_task]}
       --iter-decode-max-iter 30
       --iter-decode-eos-penalty 1
-   --max-tokens ${CONF[max_tokens]}
+   --max-tokens ${CONF[io_max_tokens]}
    --skip-invalid-size-inputs-valid-test
-   --max-source-positions ${CONF[max_source]}
+   --max-source-positions ${CONF[io_max_src]}
    --min-len 5
    --system ${OUT_DIR}/${SYSTEM}
    --reference ${OUT_DIR}/${REFERENCE}
    --truncate-source
    --user-dir ${USER_DIR}
+   --source-lang ${CONF[lang_src]}
+   --target-lang ${CONF[lang_tgt]}
 )
 if ${CONF[fp16]}; then
   OPTIONAL_ARGS+='--fp16'
@@ -94,7 +81,7 @@ echo "python ${EXEC_GEN_FILE_PATH}generate.py ${GEN_DATA_DIR} ${OPTIONAL_ARGS}"
 python ${EXEC_GEN_FILE_PATH}generate.py ${GEN_DATA_DIR} ${OPTIONAL_ARGS}
 unset CONF
 
-if [ ${CONF[data]: -7} = subword ]; then
+if [[ ${CONF[data]: -7} = subword ]]; then
   CURRENT_DIR=`pwd`
   cd $OUT_DIR
   sed -r 's/(@@ )|(@@ ?$)//g' system_output-subword.txt > system_output.txt
